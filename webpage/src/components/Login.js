5025bf702a28340d1eaa40a756ff3eb456182e3d
aggelostais <aggelosstaisv@gmail.com>
2021-02-25 20:39:34 +0200

    Add login and password fields

--- webpage/src/components/Login.js
+++ webpage/src/components/Login.js
@@ -7,6 +7,19 @@ class Login extends Component {
       <div>
         <h1>Login Page</h1>
         <nav>
+            <form>
+          <label>
+            <p>Username</p>
+            <input type="text" />
+          </label>
+          <label>
+            <p>Password</p>
+            <input type="password" />
+          </label>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
           <button>
             <Link to="/MainPage">Return Home</Link>
           </button>
