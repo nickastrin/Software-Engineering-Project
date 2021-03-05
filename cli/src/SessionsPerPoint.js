8aa10c4b325aecc7b1fcd6422296f42146eb4b44
Odysseas Chlapanis <odysseashlap@2lykko.gr>
2021-03-05 14:01:00 +0200

    Updated SessionsPerPoint.js

--- cli/src/SessionsPerPoint.js
+++ cli/src/SessionsPerPoint.js
@@ -1,6 +1,6 @@
 const https = require('https');
 const fs = require('fs');
-const { parse } = require('json2csv');
+const ObjectsToCsv = require('objects-to-csv');
 
 function sessionsPerPoint (pointid, from, to, format) {
     const url = '/evcharge/api/SessionsPerPoint/' + pointid + '/' + from + '/' + to + '?format=' + format;
@@ -10,10 +10,10 @@ function sessionsPerPoint (pointid, from, to, format) {
         console.log('User authentication required. Please sign in');
         process.exit();
     }
-
+//------------------------------------------------
     const raw = fs.readFileSync(path);
     const token = JSON.parse(raw).token;
-
+//------------------------------------
     const options = {
         hostname: 'localhost',
         port: 8765,
@@ -31,18 +31,24 @@ function sessionsPerPoint (pointid, from, to, format) {
     
         res.on('data', d=> {
             if(res.statusCode == 200) {
-                console.log('Status code: ' + res.statusCode);
-                if (format == 'json') console.log(JSON.parse(d));
-                else {
-                    console.log(parse(d));
+                if(format==='json'){
+                    //process.stdout.write(d+'\n')
+                    obj=JSON.parse(d)
+                    console.log(obj);
+                }
+                else if (format==='csv'){
+                    process.stdout.write(d+'\n')
+                }
+                else{
+                    console.log(`Format "${format}" is not recognised as a format type. \
+Accepted formats are "json" and "csv".`)
                 }
             }
             else if(res.statusCode == 401) {
                 console.log('User authentication failed.');
                 if(fs.existsSync(path)) {
-                    fs.unlink(path, () => {
-                        console.log('You have been logged out. Please log in again');
-                    });   
+                    fs.unlink(path);
+                    console.log('You have been logged out. Please log in again');
                 }
             }
             else if(res.statusCode == 402) {
